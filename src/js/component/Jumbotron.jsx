39543dec61595bfc118fb528f7cc1c4341909ae7
Jesus D Santisteban <jesus.santisteban@gmail.com>
2023-09-20 00:58:04 -0500

    Create Jumbotron.jsx

---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Jumbotron = () => {
+    return (
+        <div className="bg-light p-5 mb-3">
+            <h1 className="display-4">A Warm Welcome!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi repellat fuga veniam ipsa, similique facere praesentium velit officiis soluta excepturi, blanditiis consectetur quam maiores, inventore voluptate illum debitis repellendus sequi.
+            Aperiam, itaque quibusdam, suscipit libero pariatur consequuntur dicta sequi perferendis esse ipsum.</p>
+            <button type="button" class="btn btn-primary">Call to action!</button>
+        </div>
+    )
+}
+
+export default Jumbotron;
